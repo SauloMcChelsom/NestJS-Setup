cf1e78707bb87c89509171259d9972a64b68ebeb
Saulo Siva <saulo.silva@vixsystem.com.br>
2021-12-12 22:00:27 -0300

    refatoracao validacao public e auth

--- src/modules/comment/comment.model.ts
+++ src/modules/comment/comment.model.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository} from '@nestjs/typeorm'
 import { code, message } from '@shared/enum'
-import { UpdateDto } from './dto/update.dto'
-import { 
-  InternalServerErrorExceptions,
-  NotFoundExceptions,
-} from '@service/exception'
+import { InternalServerErrorExceptions, NotFoundExceptions, ConflictExceptions } from '@service/exception'
 import { CommentRepository } from './comment.repository'
-
+import { UpdateDto } from './dto/update.dto'
 
 @Injectable()
 export class CommentModel {
@@ -16,102 +12,103 @@ export class CommentModel {
 
   public async create(comment:any){
     try{
-        const res = await this.repository.save(comment)
-        if(res){
-            return res
-        }
-        throw true
-    }catch(error){
-        if(error == true){
-            throw new NotFoundExceptions({
-              code:code.NOT_FOUND,
-              message:message.NOT_FOUND,
-            })
-        }
-        throw new InternalServerErrorExceptions({
-            code:code.ERROR_GENERIC,
-            message:message.ERROR_GENERIC,
-            description:"algo aconteceu em verifica se esta seguindo"+` ::: ${error}`
+      const res = await this.repository.save(comment)
+      if(res){
+        return res
+      }
+      throw 'error'
+    }catch(Exception){
+      if(Exception == 'error'){
+        throw new NotFoundExceptions({
+          code:code.NOT_FOUND,
+          message:message.NOT_FOUND,
         })
+      }
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
     }
   }
 
   public async findOneByUserIdAndPublicationId(publicationId:string, userId:string){
-      try{
-          const res = await this.repository.findOne({ where:{ publication_id: publicationId, user_id:userId }})
-          if(res){
-              return res
-          }
-          throw true
-      }catch(error){
-          if(error == true){
-              throw new NotFoundExceptions({
-                code:code.NOT_FOUND,
-                message:message.NOT_FOUND,
-              })
-          }
-          throw new InternalServerErrorExceptions({
-              code:code.ERROR_GENERIC,
-              message:message.ERROR_GENERIC,
-              description:"algo aconteceu em verifica se esta seguindo"+` ::: ${error}`
-          })
+    try{
+      const res = await this.repository.findOne({ where:{ publication_id: publicationId, user_id:userId }})
+      if(res){
+        return res
       }
+      throw 'error'
+    }catch(Exception){
+      if(Exception == 'error'){
+        throw new NotFoundExceptions({
+          code:code.NOT_FOUND,
+          message:message.NOT_FOUND,
+        })
+      }
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
+    }
   }
 
   public async findOneById(id:string){
     try{
-        const res = await this.repository.findOne(id)
-        if(res){
-            return res
-        }
-        throw true
-    }catch(error){
-        if(error == true){
-            throw new NotFoundExceptions({
-              code:code.NOT_FOUND,
-              message:message.NOT_FOUND,
-            })
-        }
-        throw new InternalServerErrorExceptions({
-            code:code.ERROR_GENERIC,
-            message:message.ERROR_GENERIC,
-            description:"algo aconteceu em verifica se esta seguindo"+` ::: ${error}`
+      const res = await this.repository.findOne(id)
+      if(res){
+        return res
+      }
+      throw 'error'
+    }catch(Exception){
+      if(Exception == 'error'){
+        throw new NotFoundExceptions({
+          code:code.NOT_FOUND,
+          message:message.NOT_FOUND,
         })
+      }
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
     }
   }
 
   public async findByUserId(userId:string){
     try{
-        //const res = await this.repository.find({ where:{ user_id: userId }})
-        const res = await this.repository.find({
-          select: ["comment", "id"],
-         
-          where: {
-            user_id: userId
-          },
-          order: {
-            id: "ASC",//DESC ASC
-          },
-          skip: 3,
-          take: 3,
-          cache: true,
-        });
-        if(Object.keys(res).length != 0){
-          return res
-        }
-        throw true
-    }catch(error){
-        if(error == true){
-            throw new NotFoundExceptions({
-              code:code.NOT_FOUND,
-              message:message.NOT_FOUND,
-            })
-        }
-        throw new InternalServerErrorExceptions({
-            code:code.ERROR_GENERIC,
-            message:message.ERROR_GENERIC,
-            description:"algo aconteceu em verifica se esta seguindo"+` ::: ${error}`
+      const res = await this.repository.find({
+        select: ["comment", "id"],
+        
+        where: {
+          user_id: userId
+        },
+
+        order: {
+          id: "ASC",//DESC ASC
+        },
+
+        skip: 3,
+        take: 3,
+        cache: true,
+      });
+      if(Object.keys(res).length != 0){
+        return res
+      }
+      throw 'error'
+    }catch(Exception){
+      if(Exception == 'error'){
+        throw new NotFoundExceptions({
+          code:code.NOT_FOUND,
+          message:message.NOT_FOUND,
         })
+      }
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
     }
   }
 
@@ -121,51 +118,74 @@ export class CommentModel {
         if(Object.keys(res).length != 0){
           return res
         }
-        throw true
-    }catch(error){
-        if(error == true){
-            throw new NotFoundExceptions({
-              code:code.NOT_FOUND,
-              message:message.NOT_FOUND,
-            })
-        }
-        throw new InternalServerErrorExceptions({
-            code:code.ERROR_GENERIC,
-            message:message.ERROR_GENERIC,
-            description:"algo aconteceu em verifica se esta seguindo"+` ::: ${error}`
+        throw 'error'
+    }catch(Exception){
+      if(Exception == 'error'){
+        throw new NotFoundExceptions({
+          code:code.NOT_FOUND,
+          message:message.NOT_FOUND,
         })
+      }
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
     }
   }
 
   public async deleteById(id:string){
     try{
-        const res = await this.repository.delete(id);
-        if(res){
-            return res
-        }
-        throw res
-    }catch(error){
-        throw new InternalServerErrorExceptions({
-            code:code.ERROR_GENERIC,
-            message:message.ERROR_GENERIC,
-            description:"algo aconteceu em verifica se esta seguindo"+` ::: ${error}`
-        })
+      const res = await this.repository.delete(id);
+      if(res){
+        return res
+      }
+      throw res
+    }catch(Exception){
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
     }
   }
 
   public async updateById(id:string, body: UpdateDto) {
-      try{
-          const res = await this.repository.update(id, { ...body as any });
-        if(res){
-          return res
-        }
-      }catch(error){
-        throw new InternalServerErrorExceptions({
-          code:code.ERROR_GENERIC,
-          message:message.ERROR_GENERIC,
-          description:"algo aconteceu em atualizar updateAmFollowing"+` ::: ${error}`
+    try{
+      const res = await this.repository.update(id, { ...body as any });
+      if(res){
+        return res
+      }
+    }catch(Exception){
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em atualizar updateAmFollowing"+` ::: ${Exception}`
+      })
+    }
+  }
+
+  public async validateID(id:string, userId:string){
+    try{
+      const res = await this.repository.findOne({ where:{ id: id, user_id:userId }})
+      if(res){
+        return true
+      }
+      throw 'conflict'
+    }catch(Exception){
+      if(Exception == 'conflict'){
+        throw new ConflictExceptions({
+          code:code.DATA_CONFLICT,
+          message:message.DATA_CONFLICT,
         })
       }
+      throw new InternalServerErrorExceptions({
+        code:code.ERROR_GENERIC,
+        message:message.ERROR_GENERIC,
+        description:"algo aconteceu em verifica se esta seguindo"+` ::: ${Exception}`
+      })
+    }
   }
+  
 }
 
