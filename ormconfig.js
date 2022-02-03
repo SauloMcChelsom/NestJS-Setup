620b05f4bc8953dbe0ca346a8d39faad8c929851
Saulo Siva <saulo.silva@vixsystem.com.br>
2022-02-02 23:10:28 -0300

    orm

--- ormconfig.js
+++ ormconfig.js
@@ -1,36 +1,28 @@
-
-if(process.env.TYPEORM_HOST === '127.0.0.1'){
-  module.exports = {
-    name: 'default',
-    type: process.env.TYPEORM_TYPE,
-    host: process.env.TYPEORM_HOST,
-    port: Number(process.env.TYPEORM_PORT),
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database:  process.env.TYPEORM_DATABASE,
-    synchronize: process.env.TYPEORM_SYNCHRONIZE,
-    dropSchema: false,
-    logging: true,
-    entities: [ 'dist/**/*.entity{.ts,.js}' ],
-  };
-}else{
-  module.exports = {
-    name: 'default',
-    type: process.env.TYPEORM_TYPE,
-    host: process.env.TYPEORM_HOST,
-    port: Number(process.env.TYPEORM_PORT),
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database:  process.env.TYPEORM_DATABASE,
-    synchronize: process.env.TYPEORM_SYNCHRONIZE,
-    dropSchema: false,
-    logging: true,
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    },
-    entities: ['**/*.entity.js'],
-  };
+let ENV = {
+  name: 'default',
+  type: process.env.TYPEORM_TYPE,
+  host: process.env.TYPEORM_HOST,
+  port: Number(process.env.TYPEORM_PORT),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database:  process.env.TYPEORM_DATABASE,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE,
+  dropSchema: false,
+  logging: false,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+  entities: ['**/*.entity.js'],
 }
+
+const env = process.env.environment
+
+if(env === 'development'){
+  delete ENV.ssl
+  delete ENV.extra
+}
+
+module.exports = ENV
