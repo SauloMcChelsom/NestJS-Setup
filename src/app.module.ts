1d2157088d4775759e65236605aec6e667c9dc88
Saulo Siva <saulo.silva@vixsystem.com.br>
2022-03-14 21:56:21 -0300

    ref

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,16 +29,68 @@ import { SeniorModule } from '@modules/senior/senior.module';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    CacheModule.register(option.cache()),
-    ConfigModule.forRoot(option.typeorm()),
+    CacheModule.register({
+      ttl: 0.1, // seconds
+      max: 15, // maximum number of items in cache
+      isGlobal: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: false,
+      expandVariables: true,
+      ignoreEnvFile: false,
+      cache: true,
+    }),
     TypeOrmModule.forRoot(),
-    BullModule.forRootAsync({ useFactory: () => option.redis() }),
-    EventEmitterModule.forRoot(option.eventEmitter()),
-    MulterModule.register(option.multer()),
-    HttpModule.register(option.http()),
+    BullModule.forRootAsync({ 
+      useFactory: () => {
+        const REDIS_URL = {
+          redis: {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT),
+            password: process.env.REDIS_PASSWORD,
+            tls: {
+              rejectUnauthorized: false,
+            },
+          },
+        };
+      
+        const env = process.env.environment;
+      
+        if (env === 'development' || env === 'tests') {
+          delete REDIS_URL.redis.tls;
+          delete REDIS_URL.redis.password;
+        }
+      
+        return REDIS_URL;
+      }
+    }),
+    EventEmitterModule.forRoot({
+      // set this to `true` to use wildcards
+      wildcard: true,
+      // the delimiter used to segment namespaces
+      delimiter: '.',
+      // set this to `true` if you want to emit the newListener event
+      newListener: false,
+      // set this to `true` if you want to emit the removeListener event
+      removeListener: false,
+      // the maximum amount of listeners that can be assigned to an event
+      maxListeners: 10,
+      // show event name in memory leak message when more than maximum amount of listeners is assigned
+      verboseMemoryLeak: false,
+      // disable throwing uncaughtException if an error event is emitted and it has no listeners
+      ignoreErrors: false,
+    }),
+    MulterModule.register( {
+      dest: './CDN',
+    }),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
     ThrottlerModule.forRoot({
       ttl: 60,
-      limit: 10,
+      limit: 100,
     }),
     TasksModule,
     JobsModule,
--- src/conf/options/options.conf.ts
+++ src/conf/options/options.conf.ts
@@ -1,75 +1,3 @@
-const eventEmitter = () => {
-  return {
-    // set this to `true` to use wildcards
-    wildcard: true,
-    // the delimiter used to segment namespaces
-    delimiter: '.',
-    // set this to `true` if you want to emit the newListener event
-    newListener: false,
-    // set this to `true` if you want to emit the removeListener event
-    removeListener: false,
-    // the maximum amount of listeners that can be assigned to an event
-    maxListeners: 10,
-    // show event name in memory leak message when more than maximum amount of listeners is assigned
-    verboseMemoryLeak: false,
-    // disable throwing uncaughtException if an error event is emitted and it has no listeners
-    ignoreErrors: false,
-  };
-};
-
-const redis = () => {
-  const REDIS_URL = {
-    redis: {
-      host: process.env.REDIS_HOST,
-      port: parseInt(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASSWORD,
-      tls: {
-        rejectUnauthorized: false,
-      },
-    },
-  };
-
-  const env = process.env.environment;
-
-  if (env === 'development' || env === 'tests') {
-    delete REDIS_URL.redis.tls;
-    delete REDIS_URL.redis.password;
-  }
-
-  return REDIS_URL;
-};
-
-const cache = () => {
-  return {
-    ttl: 0.1, // seconds
-    max: 15, // maximum number of items in cache
-    isGlobal: true,
-  };
-};
-
-const typeorm = () => {
-  return {
-    envFilePath: `.env.${process.env.NODE_ENV}`,
-    isGlobal: false,
-    expandVariables: true,
-    ignoreEnvFile: false,
-    cache: true,
-  };
-};
-
-const multer = () => {
-  return {
-    dest: './CDN',
-  };
-};
-
-const http = () => {
-  return {
-    timeout: 5000,
-    maxRedirects: 5,
-  };
-};
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 const connectionDataBaseForTest = () => [
   TypeOrmModule.forRoot({
@@ -85,4 +13,4 @@ const connectionDataBaseForTest = () => [
   }),
 ];
 
-export { redis, eventEmitter, cache, typeorm, multer, http, connectionDataBaseForTest };
+export { connectionDataBaseForTest };
