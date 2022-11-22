34ee8a623f5bb9638ea30b4371020aeda0a5d150
saulo <2033.xyz@gmail.com>
2022-11-21 22:50:08 -0300

    ValidateAnswerCorrect ok

--- src/controller/quiz/quiz.service.ts
+++ src/controller/quiz/quiz.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { code } from '@root/src/shared/enum';
 import { TitleEntityModel } from '@root/src/model/quiz-entity/title/title-entity.model'
 import { QuestionEntityModel } from '@root/src/model/quiz-entity/question/question-entity.model'
 import { ResponseEntityModel } from '@root/src/model/quiz-entity/response/response-entity.model'
@@ -13,10 +14,68 @@ export class QuizService {
   ) {}
 
   public async create(body: CreateDTO) {
+    this.ValidateAnswerCorrect(body)
     body = await this.createTitle(body);
     body = await this.createQuestion(body);
     body = await this.createResponse(body);
     return body
+  }
+
+  private ValidateAnswerCorrect(body: CreateDTO){
+    let response = []
+    for (const key in body.question) {
+      for (const id in body.question[key].response) {
+        body.question[key].response[id].question_id = body.question[key].id
+        let element = body.question[key].response[id].correct ? 'ok' : 'err'
+        response.push(element)
+      }
+      if(!this.CountAnswerCorrect(response)){
+        break
+      }
+      response = []
+    }
+  }
+
+  private CountAnswerCorrect(arr){
+
+    type Counts = { ok:number, err:number }
+
+    let counts:Counts = {
+      ok:0, 
+      err:0
+    };
+
+    for (let num of arr) {
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    if(counts.ok == 0){
+      throw new HttpException({
+        code : "CORRECT_FAILURE",
+        message : `Você marcou todas falsas`,
+        description : ''
+      }, HttpStatus.BAD_REQUEST)
+    }
+
+    if(counts.err == 0 ){
+      throw new HttpException({
+        code : "CORRECT_FAILURE",
+        message : `Você marcou todas verdadeiras`,
+        description : ''
+      }, HttpStatus.BAD_REQUEST)
+    }
+
+    if(counts.ok > 1 ){
+      throw new HttpException({
+        code : "CORRECT_FAILURE",
+        message : `Você marcou mais de uma verdadeira`,
+        description : ''
+      }, HttpStatus.BAD_REQUEST)
+    }
+
+    if(counts.err == 1 || counts.ok == 1){
+      return true
+    }
   }
 
   private async createResponse(body: CreateDTO): Promise<CreateDTO>{
